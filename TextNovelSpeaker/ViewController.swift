e84c2c58c097d907468cabd5f65aad222c08c54a
IIMURA Takuji <uirou.j+git@gmail.com>
2014-06-03 23:03:48 +0900

    Initial Commit

---
+++ TextNovelSpeaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextNovelSpeaker
+//
+//  Created by 飯村 卓司 on 2014/06/03.
+//  Copyright (c) 2014年 IIMURA Takuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
